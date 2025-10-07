33fc843d1c77d88491a2a3c7172b8762290117d4
D-Michiels <daltonmichiels@yahoo.com>
2025-10-07 11:37:37 -0500

    Rewrite of Main
    Just decided to restart from scratch to see how it looks.

--- Portfolio/dmportfolio/src/components/ThemeToggle.jsx
+++
@@ -1,9 +0,0 @@
-import { Sun, Moon } from "lucide-react";
-import { useState } from "react";
-
-export const ThemeToggle = () => {
-    const [isDarkMode, SetISDarkMode] = useState(false);
-
-    return <button> {isDarkMode ? (<Sun className="h-6 w-6 text-yellow-300"/>)
-         : (<Moon />)}</button>;
-};
